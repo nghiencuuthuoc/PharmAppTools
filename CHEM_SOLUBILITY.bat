4b188643371f61cb2224fa654f04835129b17f32
Nghien Cuu Thuoc <5343795+nghiencuuthuoc@users.noreply.github.com>
2024-02-29 10:04:27 +0700

    Add files via upload

---
+++ CHEM_SOLUBILITY.bat
@@ -0,0 +1,18 @@
+@echo off
+mode con: cols=100 lines=20
+Title "SOLUBILITY DATA"
+color A1
+@echo on
+@ echo:++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+@ Echo: PharmApp // Copyright 2024 // NGHIEN CUU THUOC // RnD PHARMA PLUS // WWW.NGHIENCUUTHUOC.COM
+@ Echo: Email: nghiencuuthuoc@gmail.com // Facebook: https://facebook.com/nghiencuuthuoc
+@ echo:++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+@echo off
+
+set /p key_search=ENTER KEY OF SOLUBILITY DATA DO YOU WANT SEARCH:
+
+start "" "https://www.google.com.vn/search?q=%key_search%+solubility"
+start "" "https://www.google.com.vn/search?q=%key_search%+pka
+
+CHEM_SOLUBILITY.bat
+
